b4d7fb0a7c05ef9eb140ec9b09e1b81407da3fe2
julio <julio>
2025-03-27 21:33:36 -0300

    adding components and reusing with differents props

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
-const reactDescriptions = ['Fundamental', 'Crucial', 'Core']
+import reactImg from './assets/react-core-concepts.png';
+import { CORE_CONCEPTS } from './data.js'
+
+const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
 
 function genRandomInt(max) {
   return Math.floor(Math.random() * (max + 1));
 }
 
 function Header() {
-  const description =  reactDescriptions[genRandomInt(2)]
+  const description =  reactDescriptions[genRandomInt(2)];
 
   return (
     <header>
-      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+      <img src={reactImg} alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
         {description} React concepts you will need for almost any app you are
@@ -19,11 +22,28 @@ function Header() {
   )
 }
 
+function CoreConcept({image, title, description}) {
+  return <li>
+    <img src={image} alt={title} />
+    <h3>{title}</h3>
+    <p>{description}</p>
+  </li>
+}
+
 function App() {
   return (
     <div>
       <Header />
       <main>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcept {...CORE_CONCEPTS[0]} />
+            <CoreConcept {...CORE_CONCEPTS[1]} />
+            <CoreConcept {...CORE_CONCEPTS[2]} />
+            <CoreConcept {...CORE_CONCEPTS[3]} />
+          </ul>
+        </section>
         <h2>Time to get started!</h2>
       </main>
     </div>
