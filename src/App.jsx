c38e171b7ae0df276834b8568a579aaf6ee4501d
julionog96 <julio>
2025-04-09 23:53:42 -0300

    separating features in files

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,18 @@
-import { useState } from 'react';
-
-import { CORE_CONCEPTS } from './data.js'
 import Header from './components/Header/Header.jsx';
-import CoreConcept from './components/CoreConcept.jsx';
-import TabButton from './components/TabButton.jsx';
+import CoreConcepts from './components/CoreConcepts.jsx';
+import Examples from './components/Examples.jsx';
+
 
 function App() {
-  const [ selectedTopic, setSelectedTopic ] = useState('Please click a button');
-
-  function handleSelect(selectedButton) {
-    // selectedButton => 'components', 'JSX', 'props', 'state'
-    setSelectedTopic(selectedButton);
-}
 
   return (
-    <div>
+    <>
       <Header />
       <main>
-        <section id="core-concepts">
-          <h2>Core Concepts</h2>
-          <ul>
-            <CoreConcept {...CORE_CONCEPTS[0]} />
-            <CoreConcept {...CORE_CONCEPTS[1]} />
-            <CoreConcept {...CORE_CONCEPTS[2]} />
-            <CoreConcept {...CORE_CONCEPTS[3]} />
-          </ul>
-        </section>
-        <section id="examples">
-          <h2>Examples</h2>
-          <menu>
-            <TabButton onSelect={() => handleSelect('components')}>Components</TabButton>
-            <TabButton onSelect={() => handleSelect('jsx')}>JSX</TabButton>
-            <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
-            <TabButton onSelect={() => handleSelect('components')}>State</TabButton>
-          </menu>
-          {selectedTopic}
-        </section>
-        <h2>Time to get started!</h2>
+        <CoreConcepts />
+        <Examples />
       </main>
-    </div>
+    </>
   );
 }
 
